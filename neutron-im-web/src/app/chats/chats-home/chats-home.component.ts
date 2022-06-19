e06babe2dd8789c7a59ed1dc3d2284335de66c6c
zhangjun <763653451@qq.com>
2022-06-19 22:15:28 +0800

    fix(web): rename css to less

--- neutron-im-web/src/app/chats/chats-home/chats-home.component.ts
+++ neutron-im-web/src/app/chats/chats-home/chats-home.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nim-chats-home',
   templateUrl: './chats-home.component.html',
-  styleUrls: ['./chats-home.component.css']
+  styleUrls: ['./chats-home.component.less']
 })
 export class ChatsHomeComponent {}
--- neutron-im-web/src/app/layout/error/error.component.ts
+++ neutron-im-web/src/app/layout/error/error.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nim-error',
-  templateUrl: './error.component.html'
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.less']
 })
 export class ErrorComponent {}
